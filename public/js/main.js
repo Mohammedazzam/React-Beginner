a0c9e7eefc3e3ac37a52cee7bd7ff17eca317d5a
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2022-02-12 18:17:26 +0200

    counter & Data Binding

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,41 @@
 "use strict";
 
-// var title = "test";
+// class Hello extends React.Component {
+//     render(){
+//         //return JSX
+//         return <div>Hello Component</div>
+//     }
+// }
 
-// var ele = (
-//     <div>
-//         <h2>This Is {title ? title : "Title"} item</h2>
-//         <span>{title && "Yes"}</span>
+// class Header extends React.Component {
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Header Component</h1>
+//                 <span>Header 1</span>
+//                 <span>Header 2</span>
+//             </div>
+//         )
+//     }
+// }
+
+// let content  = (
+//     <div className= "">
+//         <Hello/>
+//         <p>This Is P</p>
+//         <Header/>
 //     </div>
 // )
 
+// ReactDOM.render(content, document.getElementById("app"))
+
+/****************************************/
 
 var number = 0;
 
 var increse = function increse() {
     number++;
-    console.log("number", number);
+    // console.log(number)
     render();
 };
 
@@ -27,11 +48,9 @@ function render() {
         React.createElement(
             "button",
             { onClick: increse },
-            "Add One"
+            " add one"
         )
     );
-
     ReactDOM.render(ele, document.getElementById("app"));
 }
-
 render();
--- src/index.js
+++ src/index.js
@@ -1,31 +1,51 @@
-// var title = "test";
 
-// var ele = (
-//     <div>
-//         <h2>This Is {title ? title : "Title"} item</h2>
-//         <span>{title && "Yes"}</span>
+// class Hello extends React.Component {
+//     render(){
+//         //return JSX
+//         return <div>Hello Component</div>
+//     }
+// }
+
+// class Header extends React.Component {
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Header Component</h1>
+//                 <span>Header 1</span>
+//                 <span>Header 2</span>
+//             </div>
+//         )
+//     }
+// }
+
+// let content  = (
+//     <div className= "">
+//         <Hello/>
+//         <p>This Is P</p>
+//         <Header/>
 //     </div>
 // )
+
+// ReactDOM.render(content, document.getElementById("app"))
+
+/****************************************/
 
 
 var number = 0;
 
-var increse = () =>{
-    number++;
-    console.log("number", number)
+var increse = () => {
+    number++
+    // console.log(number)
     render()
 }
 
 function render(){
-    var ele = (
+    var ele =(
         <div>
             The Number {number}
-            <button onClick={increse}>Add One</button>
+            <button onClick={increse}> add one</button>
         </div>
     )
-
-
-ReactDOM.render(ele, document.getElementById("app"))
+    ReactDOM.render(ele, document.getElementById("app"))
 }
-
-render();
+render()
